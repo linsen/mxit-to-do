b36e42e816b89ac9c699fb66484460c987ea410d
Linsen Loots <linsen.loots@gmail.com>
2012-09-17 14:42:39 +0200

    First commit - vanilla empty rails app

---
+++ README.md
@@ -0,0 +1,4 @@
+Mxit To-Do
+==========
+
+A simple sample app that utilises the [mxit-rails](https://github.com/linsen/mxit-rails) gem.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MxitToDo::Application.config.secret_token = '8b548f8119e43d1c51bff4e02b8117b13a02c66a2fca3c6fd8af09b9ec82d2b2ed572eeaac0e951bd48999f1c2ac9033d972056d26a7e42ebb06f5c48d9d350c'
